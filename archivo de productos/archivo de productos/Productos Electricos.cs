38d19b8423af9db1a29f24db5c5e80e12cd25d47
Calix1402 <88732337+Calix1402@users.noreply.github.com>
2021-08-29 19:59:56 -0600

    Add files via upload

---
+++ archivo de productos/archivo de productos/Productos Electricos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace manejoArchivos
+{
+    class Archivos
+    {
+        static void Main()
+        {
+            bool showMenu = true;
+
+            while (showMenu)
+            {
+                showMenu = Menu();
+            }
+            Console.ReadKey();
+        }
+
+        private static bool Menu()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Seleccion la operación a realizar: ");
+            Console.WriteLine("1. Registrar nuevo producto");
+            Console.WriteLine("2. Actualizar datos de producto");
+            Console.WriteLine("3. Mostrar listado de productos");
+            Console.WriteLine("4. Salir");
+            Console.Write("\nOpcion: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    registro();
+                    return true;
+
+                case "2":
+                    return true;
+
+                case "3":
+                    lectura();
+                    return true;
+
+                default:
+                    return false;
+
+                case "4":
+                    return false;
+            }
+        }
+
+        private static string getPath()
+        {
+            string path = @"C:\Users\alexi\OneDrive\Escritorio\Proyectos\archivo de productos\Archivos.txt";
+            return path;
+        }
+
+
+        private static void registro()
+        {
+            Console.WriteLine("ingrese el nuevo producto");
+            Console.Write("nombre del producto: ");
+            string nombre = Console.ReadLine();
+            Console.Write("precio: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+
+            using (StreamWriter sw = File.AppendText(getPath()))
+            {
+                sw.WriteLine("{0}; ${1}", nombre, precio);
+                sw.Close();
+            }
+        }
+
+        private static void lectura()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\alexi\OneDrive\Escritorio\Proyectos\archivo de productos\Archivos.txt");
+            System.Console.WriteLine("Contenido del archivo: ");
+            foreach (string line in lines)
+            {
+
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("Presione cualquier tecla para regresar al menu.");
+            System.Console.ReadKey();
+        }
+
+
+             
+    }
+}
